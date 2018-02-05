fcb8377b44b3985d01dde8927f08a4c77bba8799
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2018-02-05 16:35:39 +0100

    added functions and manual test

--- include/Transformer.h
+++ include/Transformer.h
@@ -1,59 +1,120 @@
-#include<Eigen/Dense>
+#ifndef _TRANSFORMER_H_
+#define _TRANSFORMER_H_
+
+#include<eigen3/Eigen/Dense>
 
 namespace esrocos {
   namespace transformer{
 
-    template<int numberOfFrames = 20, int stringSize = 256>
+    template<unsigned int numberOfFrames = 20, unsigned int stringSize = 20>
     class AcyclicTransformer{
 
-    private:
-
-      static const int frames_ = numberOfFrames;
-      static const int ssize_ = stringSize;
-
-      char frames[numberOfFrames*stringSize];
-
-      Eigen::Matrix<double, numberOfFrames-1,4> transforms;
-
-    public:
-
-      int frames() const {return frames_;}
-      int ssize() const {return ssize_;}
-
-      std::string frameNameAt(int i){
-        char tmp[stringSize] = frames + sizeof(char)*stringSize*i;
-        return std::string(tmp);
-      }
-
-      // plus one for the null terminator
-      bool setFrameNameAt(std::string s, int i){
-        // if the string is too long, do nothing and return false
-        if (s.length()+1 > stringSize) return false;
-        // does that work?
-        std::strcpy(frames+sizeof(char)*i*stringSize,s.c_str());
-
-        return true;
-      }
-
-      class Frame {
-
-      };
+      public:
 
       class Transformation{
 
       public:
 
-        char name[stringSize];
-        char from[stringSize];
-        char to[stringSize];
+        char id[stringSize];
 
-        Eigen<double,4,4> transformation;
+        char a[stringSize];
+        char b[stringSize];
+
+        Eigen::Matrix<double,4,4> atob;
+        Eigen::Matrix<double,4,4> btoa;
 
       private:
-
       };
+
+      class Frame {
+      public:
+        char id[stringSize];
+        Transformation transformToParent;
+      };
+
+
+      int frames() const {return maxFrames_;}
+      int ssize() const {return maxStringSize_;}
+
+      AcyclicTransformer(Frame root){
+        if(currentFrames_ == maxFrames_) return;
+
+        frames_[currentFrames_] = root;
+        currentFrames_++;
+
+        return;
+      }
+
+      bool addFrame(Frame f){
+        if(currentFrames_ == maxFrames_) return false;
+
+        frames_[currentFrames_] = f;
+        currentFrames_++;
+
+        addTransformation(f.transformToParent);
+
+        return true;
+      }
+
+      bool getFrame(char * id, Frame & f){
+        for(int i = 0; i < maxFrames_; i++){
+          if (std::strcmp(id,frames_[i].id) == 0) {
+            f = frames_[i];
+            return true;
+          }
+        }
+        return false;
+      }
+
+      bool getTransform(char * frame_a, char * frame_b, Transformation& t){
+        //TODO
+        return false;
+      }
+
+      bool updateTransform(char * id, Transformation t){
+        for(int i = 0; i < maxFrames_-1; i++){
+          if (std::strcmp(id,transforms_[i].id) == 0) {
+            t = transforms_[i];
+            return true;
+          }
+        }
+        return false;
+      }
+
+    private:
+
+      AcyclicTransformer(){}
+
+      bool addTransformation(Transformation t){
+        if(currentTransformations_ == maxTransforms_) return false;
+
+        transforms_[currentTransformations_] = t;
+        currentTransformations_++;
+
+        return true;
+      }
+
+      bool getTransform(char * id, Transformation& t){
+        for(int i = 0; i < maxTransforms_; i++){
+          if (std::strcmp(id,transforms_[i].id) == 0) {
+            t = transforms_[i];
+            return true;
+          }
+        }
+        return false;
+      }
+
+      int currentFrames_;
+      int currentTransformations_;
+
+      static const int maxFrames_ = numberOfFrames;
+      static const int maxTransforms_ = numberOfFrames-1;
+      static const int maxStringSize_ = stringSize;
+
+      Frame frames_[numberOfFrames];
+      Transformation transforms_[numberOfFrames-1];
     };
-
-
   }
 }
+
+#endif
--- test/main.cc
+++ test/main.cc
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <Transformer.h>
 
+typedef esrocos::transformer::AcyclicTransformer<20,20> atf;
+
 int main(int argc, char ** args) {
 
-std::cout << "hello world" << std::endl;
+atf::Frame world;
 
-Transformer<10> transformer_;
+atf tf(world);
+
+atf::Frame base_link;
+atf::Frame lidar_1;
+
+
+tf.addFrame(base_link);
+tf.addFrame(lidar_1);
+
+std::cout << "hello world" << std::endl;
+std::cout << "max frames:" << tf.frames() << std::endl;
+std::cout << "string size:" << tf.ssize() << std::endl;
 
 return 1;
 }
